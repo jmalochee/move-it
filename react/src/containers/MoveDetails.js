befeaad1e308c9c4c0a2638f5bfb4b62a2440b1b
jmalochee <jmalochee@gmail.com>
2017-10-23 18:25:54 -0400

    move show component rendering without style

--- react/src/containers/MoveDetails.js
+++ react/src/containers/MoveDetails.js
@@ -1,20 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class MoveDetails extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      current_move: null
-    }
+const MoveDetails = props => {
+  let detail = (name, value) => {
+    return( <tr><td><strong>{name}:</strong></td><td>{value}</td></tr>)
   }
 
-  render() {
+  let address = (addr) => {
     return(
-      <div className="row move-details">
-        
+      <div><p>{addr.street}<br/>{addr.city}, {addr.state} {addr.zip}</p></div>
+    )
+  }
+
+  let side = (i) => {
+    return(
+      <div className="row">
+        <div className="small-12 medium-6 large-6 columns address">
+          {address(i.address)}
+        </div>
+        <div className="small-12 medium-6 large-6 columns address-details">
+          <table>
+            {detail("floor", i.floor)}
+            {detail("rooms", i.rooms)}
+            {detail("home access", i.access)}
+            {detail("truck access", i.truck_access)}
+            {detail("walking distance", i.distance)}
+          </table>
+        </div>
       </div>
     )
   }
+
+  return(
+    <div className="column move-details">
+      This move will take us...
+      <div id="origin">
+        <div className="row">
+          ...from here
+        </div>
+        {side(props.move.origin)}
+      </div>
+      <div id="destination">
+        <div className="row">
+          ...to here
+        </div>
+        {side(props.move.destination)}
+      </div>
+    </div>
+  )
 }
 
 export default MoveDetails;
